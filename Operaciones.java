e29f470f1c18c055adf8a2978da57fde581a0072
marisantiagoyupanqui-ship-it <marisantiagoyupanqui@gmail.com>
2026-04-20 08:52:33 -0500

    Add files via upload

---
+++ Operaciones.java
@@ -0,0 +1,10 @@
+public class Operaciones {
+    public static void main(String[] args){
+        int a = 20;
+        int b = 5;
+        System.out.println("Suma:"+(a+b));
+        System.out.println("Resta de:"+(a-b));
+        System.out.println("multiplicacion:"+(a*b));
+        System.out.println("Division:"+(a/b));
+    }
+}
