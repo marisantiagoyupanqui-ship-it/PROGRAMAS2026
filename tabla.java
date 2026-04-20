2111bde4a03cd37323705a4aea5918169ad17557
marisantiagoyupanqui-ship-it <marisantiagoyupanqui@gmail.com>
2026-04-20 09:28:37 -0500

    Add files via upload

---
+++ tabla.java
@@ -0,0 +1,10 @@
+public class tabla {
+    public static void main(String[] args){
+        int numero = 5; 
+        System.out.println("Tabla del"+numero);
+        for(int i =1; i<=10;i++){
+            System.out.println(numero +"x"+i+"="+(numero*i));
+        }
+    }
+    
+}
