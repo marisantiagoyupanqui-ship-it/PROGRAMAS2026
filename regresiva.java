29d378f6e3eed3a7167d8a68acec17fbdb4e7a9c
marisantiagoyupanqui-ship-it <marisantiagoyupanqui@gmail.com>
2026-04-20 09:42:24 -0500

    Add files via upload

---
+++ regresiva.java
@@ -0,0 +1,11 @@
+public class regresiva {
+    public static void main(String[] args){
+        System.out.println("Cuenta regresiva:");
+        for (int i=10; i>=1;i--){
+            System.out.println(i);
+        }
+    }
+
+
+
+}
