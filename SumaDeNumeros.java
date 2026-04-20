3be0be69d41fa804a615310ebdc6751fbf5cf055
marisantiagoyupanqui-ship-it <marisantiagoyupanqui@gmail.com>
2026-04-20 08:45:45 -0500

    Add files via upload

---
+++ SumaDeNumeros.java
@@ -0,0 +1,8 @@
+public class SumaDeNumeros {
+    public static void main(String[] args){
+        int a = 15;
+        int b = 5 ;
+        int suma = a + b;
+        System.out.print("La Suma de "+a+"+"+b+"="+suma);
+    } 
+} 
