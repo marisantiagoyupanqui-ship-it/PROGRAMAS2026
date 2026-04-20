05aabe2869bba7d6d6468a058d0f66b1529b8f77
marisantiagoyupanqui-ship-it <marisantiagoyupanqui@gmail.com>
2026-04-20 09:24:27 -0500

    Add files via upload

---
+++ Calificacion.java
@@ -0,0 +1,12 @@
+public class Calificacion {
+    public static void main(String[] args){
+        int nota = 16;
+        if( nota >=18){
+            System.out.println("Alumno destacable");
+        } else if(nota>=14){
+            System.out.println("alumno regular");
+        }else{
+            System.out.println("Alumno no destacable");
+        }
+    }    
+}
