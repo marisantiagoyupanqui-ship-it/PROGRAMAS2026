b151640a6371ab5d82f968a413058fbc06bafbcb
marisantiagoyupanqui-ship-it <marisantiagoyupanqui@gmail.com>
2026-04-20 09:10:53 -0500

    Add files via upload

---
+++ MayorDeEdad.java
@@ -0,0 +1,11 @@
+public class MayorDeEdad{
+    public static void main(String[] args){
+        int edad = 18;
+        if ( edad >=18){
+            System.out.println(" Es Mayor de edad");
+        } else{
+            System.out.println("Es menor de edad ");
+        } 
+    } 
+        }
+
